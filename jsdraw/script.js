0511082c6085ac98add334247b5a85f429e1757d
adam <adam.sosnowski.86@gmail.com>
2015-11-11 18:18:12 +0100

    rysowanie scian

--- jsdraw/script.js
+++ jsdraw/script.js
@@ -1,12 +1,48 @@
-var DrawPlan = (function () {
-	var canvas;
-	var context;
+var Draw = Draw || {};
+Draw.Mode = {
+	WALL : 1,
+	DOORL : 2,
+	DOORR : 3
+};
+Draw.Point = function(x, y) {
+	return {
+		setX: function(newX) {
+			x = newX;	
+		},
+		getX: function() {
+			return x;
+		},
+		setY: function(newY) {
+			y = newY;	
+		},
+		getY: function() {
+			return y;
+		},
+		normalize: function(p) {
+			x = Math.abs(x-p.getX());
+			y = Math.abs(y-p.getY());
+		}
+	} 
+};
+Draw.Wall = function(start, end) {
+	return {
+		getStart: function() {
+			return start;
+		},
+		getEnd: function() {
+			return end;
+		}
+	}
+};
+Draw.Plan = function () {
+	var logFunction, canvas, context, walls = [], mode = Draw.Mode.WALL, doorSize = 20, wallSize = 3;
 	var state = {
 		drawing: false,
-			start: { x: 0, y: 0},
-			end: {x: 0, y: 0 }
+		dimensionBlockade: false,
+			start: Draw.Point(0, 0),
+			end: Draw.Point(0, 0)
 		};
-	var walls = [];
+	
 	var undo = function () {
 		alert('undo');
 	};
@@ -26,14 +62,38 @@ var DrawPlan = (function () {
 		context.stroke();
 	};
 	var drawSingleLine = function(line) {
-		context.moveTo(line.start.x, line.start.y);
-    	context.lineTo(line.end.x, line.end.y);
-    	context.lineWidth = 2;	
+		context.moveTo(line.getStart().getX(), line.getStart().getY());
+    	context.lineTo(line.getEnd().getX(), line.getEnd().getY());
+    	context.lineWidth = wallSize;	
 	};
 	var drawLine = function(line) {
 		context.beginPath();
 		drawSingleLine(line);
     	context.stroke();
+	};
+	var drawDoor = function(point, orientation) {
+		context.beginPath();
+		context.moveTo(point.getX(), point.getY());
+		//var tmp = orientation === 0 ? Draw.Point(point.getX(), doorSize + point.getY()) : Draw.Point(doorSize + point.getX(), point.getY());
+    	//context.lineTo(tmp.getX(), tmp.getY());
+		context.moveTo(point.getX(), point.getY());
+		context.arc(point.getX(), point.getY(), doorSize, 1.5*Math.PI, 1*Math.PI, true);
+		context.closePath();
+		context.stroke();
+	};
+	var getCorectedCoordinates = function(control, position) {
+		if (state.dimensionBlockade) {
+			var tmp = Draw.Point(position.getX(), position.getY());
+			console.log('b', tmp);
+			tmp.normalize(control);
+			console.log('a', tmp);
+			if (tmp.getX() > tmp.getY()) {
+				position.setY(control.getY());
+			} else {
+				position.setX(control.getX());
+			}
+		}
+		return position;
 	};
 	var getCursorCoordinates = function (e) {
 		var x, y;
@@ -47,35 +107,53 @@ var DrawPlan = (function () {
 		}
 		x -= canvas.offsetLeft;
 		y -= canvas.offsetTop;
-		return {
-			 x: x,
-			  y: y
-		 };
+		
+		return Draw.Point(x, y);
 	};
 	var click = function (e) {
 		var position = getCursorCoordinates(e);
-		if (state.drawing) {
-			state.end = position;
-			var line = {start: state.start, end: state.end};
-			drawLine(line);
-			walls.push(line);
+		if (mode === Draw.Mode.WALL) {
+			if (state.drawing) {
+				state.end = getCorectedCoordinates(state.start, position);
+				var line = Draw.Wall(state.start, state.end);
+				drawLine(line);
+				walls.push(line);
+			}
+			else {
+				state.start = position;
+			}
+			state.drawing = !state.drawing;
+		} else if (mode === Draw.Mode.DOORL) {
+			drawDoor(getCursorCoordinates(e), 0);
+		} else if (mode === Draw.Mode.DOORR) {
+			drawDoor(getCursorCoordinates(e), 1);
 		}
-		else {
-			state.start = position;
-		}
-		state.drawing = !state.drawing;
-		
 	};
 	var move = function (e) {
-		if (state.drawing) {
-			drawScene();
-			state.end = getCursorCoordinates(e);
-			drawLine({start: state.start, end: state.end});
+		if (mode === Draw.Mode.WALL) {
+			if (state.drawing) {
+				drawScene();
+				state.end = getCorectedCoordinates(state.start, getCursorCoordinates(e));
+				logPoint(state.end);
+			
+				drawLine(Draw.Wall(state.start, state.end));
+			}
+		} else if (mode === Draw.Mode.DOORL) {
+			//poszukaj najblizszej sciany
+			//oblicz orientację drzwi w stosunku do sciany
+			//rysuj drzwi
+			console.log('lewe drzwi');
+		} else if (mode === Draw.Mode.DOORR) {
+			console.log('prawe drzwi');
+		}
+	};
+	var logPoint = function(p) {
+		if (logFunction) {
+			logFunction('X: ' + p.getX() + ', Y:' + p.getY());
 		}
 	};
 	return {
 		init: function (elements) {
-			alert(elements.canvasId);
 			if (elements) {
 				if (elements.canvasId) {
 					canvas = document.getElementById(elements.canvasId);
@@ -92,6 +170,21 @@ var DrawPlan = (function () {
 					}
 				}
 			}
+		},
+		setDimensionsBlockade: function(block) {
+			state.dimensionBlockade = block;
+		},
+		setMode: function(newMode) {
+			mode = newMode;
+			console.log(mode);
+		},
+		setLogger: function(arg) {
+			if(arg && typeof arg === 'function') {
+				logFunction = arg;
+			}
 		}
 	};
-})();
+};
+//skalowanie obszaru rysowania (możliwość powiększania i przesuwania)
+//łapanie najbliższych punktów do łączenia linii
+//drzwi rysowane przez drawarc czy cos
